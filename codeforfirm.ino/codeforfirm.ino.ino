e1fcce8fbadb7beb74ef2d4946faff98cd3b742d
acharya-dilip <acharyamission6@gmail.com>
2025-07-30 17:42:56 +0545

    Added the buzzer beep thing when a button press detected

--- codeforfirm.ino/codeforfirm.ino.ino
+++ codeforfirm.ino/codeforfirm.ino.ino
@@ -9,8 +9,8 @@ int LED3 = D9;
 int LED4 = D10;
 int butSeq[4];
 void setup() {
-pinmode(SW1,INPUT_PULLUP); //Clicky Guys
-pinmode(SW2,INPU_PULLUPT);
+pinmode(SW1,INPUT_PULLUP);
+pinmode(SW2,INPUT_PULLUP);
 pinmode(SW3,INPUT_PULLUP);
 pinmode(SW4,INPUT_PULLUP);
 pinmode(BUZ,OUTPUT); //Buuz Buzz Bad Boi
@@ -18,14 +18,16 @@ pinmode(LED1,Output); //The LED Ladies
 pinmode(LED2,Output);
 pinmode(LED3,Output);
 pinmode(LED4,Output);
-randomSeed(analogRead(A0));
 unsigned int delay=200;
+bool butPressMatch=true;
 bool butStat=false;
+bool seqStatus=false;
 long startTime = millis();
 
 }
 
 void loop() {
+ randomSeed(analogRead(A0));
  long onCallsec;
  if (digitalRead(SW1)== LOW && digitalRead(SW4) == LOW){
     if(!butStat){
@@ -40,14 +42,57 @@ void loop() {
       butStat=false;
     }
   }
+  
+  delay(500);
 }
 
 void gameStart(){
 getrandobutseq();
 playbutSeq();
+if(!checksequence()){
+  digitalWrite(BUZ,HIGH);
+  delay(5000);
+  digitalWrite(BUZ,LOW);
 }
 
-void playbutSeq(){
+}
+
+bool checkSequence() {//This Function checks if the button pressed is in the right order
+  bool match = true;
+
+  for (int i = 0; i < 4; i++) {
+    int pressed = waitForButton();  //Gets the value of the pressed button
+
+    Serial.print("Pressed: ");
+    Serial.print(pressed);
+    Serial.print(" Expected: ");
+    Serial.println(pattern[i]);
+
+    if (pressed != butSeq[i]) {
+      match = false;
+      break;  // Wrong input — exit early
+    }
+  }
+
+  return match;
+}
+
+int waitForButton() {//THis Function checks if a button is pressed and returns the pressed buttons value
+  while (true) {
+    for (int i = 0; i < 4; i++) {
+      if (digitalRead(swPins[i]) == LOW) {
+        digitalWrite(BUZ,HIGHT);
+        delay(delay);
+        digitalWrite(BUZ,LOW);
+        delay(200);  // debounce
+        while (digitalRead(swPins[i]) == LOW);  // wait for release
+        return i + 1;  // return 1-based button number
+      }
+    }
+  }
+}
+
+void playbutSeq(){ //This thing lights up the buttons in random order
   for(int i=0; i<4; i++){
     switch (butSeq[i]){
       case 1:
