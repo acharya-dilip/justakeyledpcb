1e4302735ab3aa9fd30418ef2af36dd90cb740bb
acharya-dilip <acharyamission6@gmail.com>
2025-07-30 16:07:46 +0545

    Cleared the A0 pin for ard seeding for random num and moved the pin downwards

--- codeforfirm.ino/codeforfirm.ino.ino
+++ codeforfirm.ino/codeforfirm.ino.ino
@@ -2,19 +2,16 @@ int SW1 = D0;
 int SW2 = D1;
 int SW3 = D2;
 int SW4 = D3;
-int SWSTR = D5;
-
 int BUZ = D4;
-
 int LED1 = D7;
 int LED2 = D8;
 int LED3 = D9;
 int LED4 = D10;
 void setup() {
-pinmode(SW1,INPUT); //Clicky Guys
-pinmode(SW2,INPUT);
-pinmode(SW3,INPUT);
-pinmode(SW4,INPUT);
+pinmode(SW1,INPUT_PULLUP); //Clicky Guys
+pinmode(SW2,INPU_PULLUPT);
+pinmode(SW3,INPUT_PULLUP);
+pinmode(SW4,INPUT_PULLUP);
 
 pinmode(BUZ,OUTPUT); //Buuz Buzz Bad Boi
 
@@ -22,12 +19,31 @@ pinmode(LED1,Output); //The LED Ladies
 pinmode(LED2,Output);
 pinmode(LED3,Output);
 pinmode(LED4,Output);
+
+bool butStat=false;
+
 long startTime = millis();
 
 }
 
 void loop() {
- long sw1timE;
-if (digitalRead())
-
+ long onCallsec;
+ if (digitalRead(SW1)== LOW && digitalRead(SW4) == LOW){
+    if(!butStat){
+    onCallsec=millis();
+    butStat=true;
+    }
+     elseif(millis()-onCallsec > 5000){
+      gameStart();
+      butStat=false;
+    }
+      else{
+      butStat=false;
+    }
+  }
 }
+
+void gameStart(){
+  
+}
+
